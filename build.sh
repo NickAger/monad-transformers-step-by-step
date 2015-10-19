ff284b5da8cad6ffbcd87df851ea6675a19ff211
Nick Ager <nick.ager@gmail.com>
2015-10-19 21:14:06 +0100

    updated builder batch

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-# thanks to epta, this doesn't work yet but it's a place to start.
-pandoc -f latex+lhs -t latex mt-sbs.lhs -o mt-sbs.pdf
+lhs2TeX Transformers.lhs -o Transformer.tex
+pdflatex Transformers.tex
+
